f2aaee0e76c74c126e905e335aa65ebf3778b30e
SiddhuPython <40037543+SiddhuPython@users.noreply.github.com>
2018-06-29 10:07:33 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,3 +1,6 @@
-def hello_world():
-	return "hello_world"
-print(hello_world)
+def hello_world(a,b):
+	print ("Hello Siddhu This is your first build")
+	return (a+b)
+if __name__ = "__main__":
+	print(hello_world(10,2))
+	
